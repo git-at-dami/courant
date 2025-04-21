05ec1f0535681d9dd175d1caf9f3999ea373ad78
Ifedayo Damilola <sureife@gmail.com>
2025-04-21 17:28:39 +0100

    results section skeleton

--- modules/search/ui/sections/results-section.tsx
+++ modules/search/ui/sections/results-section.tsx
@@ -1,16 +1,46 @@
 "use client"
 
+import { FilterCarousel } from "@/components/filter-carousel";
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { PAGE_DEFAULT_LIMIT } from "@/lib/constants";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface ResultsSectionProps {
   query: string | undefined;
   categoryId: string | undefined;
 }
 
-export const ResultsSection = ({
+export const CategoriesSection = (props: ResultsSectionProps) => {
+  return (
+      <Suspense fallback={<ResultsSectionSkeleton/>}>
+          <ErrorBoundary fallback={<p>Error...</p>}>
+              <ResultsSectionSuspense {...props} />
+          </ErrorBoundary>
+      </Suspense>
+  )
+}
+
+const ResultsSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 md:flex">
+      { Array.from({ length: PAGE_DEFAULT_LIMIT }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-4 p-4 gap-y-10 pt-6 md:hidden">
+        { Array.from({ length: PAGE_DEFAULT_LIMIT }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const ResultsSectionSuspense = ({
   query,
   categoryId
 }: ResultsSectionProps) => {
